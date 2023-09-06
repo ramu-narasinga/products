a8bc37e97fbf9bbde61e50df0f5e2ed9adf13204
Nicoll <nicoll.g4@gmail.com>
2023-09-06 10:15:05 -0500

    adding navigation to login

--- apps/pro-nextjs/src/components/app/navigation.tsx
+++ apps/pro-nextjs/src/components/app/navigation.tsx
@@ -7,7 +7,18 @@ import {useRouter} from 'next/router'
 import {track} from '@skillrecordings/skill-lesson/utils/analytics'
 import {twMerge} from 'tailwind-merge'
 import {cn} from '@skillrecordings/ui/utils/cn'
-import {DocumentIcon} from '@heroicons/react/outline'
+import {DocumentIcon, LogoutIcon} from '@heroicons/react/outline'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@skillrecordings/ui'
+import {signOut, useSession} from 'next-auth/react'
+import Gravatar from 'react-gravatar'
+import cx from 'classnames'
 
 const useAbilities = () => {
   const {data: abilityRules} = trpc.abilities.getAbilities.useQuery()
@@ -57,16 +68,16 @@ const Navigation: React.FC<NavigationProps> = ({className}) => {
         />{' '}
         Pro Next.js
       </Link>
-      <ul className="flex items-center gap-5">
-        {links.map(({href, label, icon}) => {
-          const isCurrent = asPath === href
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-1 text-sm">
+          {links.map(({href, label, icon}) => {
+            const isCurrent = asPath === href
 
-          return (
-            <li key={href}>
+            return (
               <Link
                 href={href}
                 className={cn(
-                  'inline-flex items-center gap-1 text-sm opacity-75 transition hover:opacity-100',
+                  'inline-flex items-center gap-1 opacity-75 transition hover:opacity-100',
                   {
                     underline: isCurrent,
                   },
@@ -78,11 +89,84 @@ const Navigation: React.FC<NavigationProps> = ({className}) => {
               >
                 {icon} {label}
               </Link>
-            </li>
-          )
-        })}
-      </ul>
+            )
+          })}
+        </div>
+        <div className="flex items-center gap-1 text-sm">
+          <Login />
+          <User />
+        </div>
+      </div>
     </nav>
+  )
+}
+
+const User: React.FC<{className?: string}> = ({className}) => {
+  const {pathname} = useRouter()
+  const {data: sessionData, status: sessionStatus} = useSession()
+  const isLoadingUserInfo = sessionStatus === 'loading'
+
+  return (
+    <>
+      {isLoadingUserInfo || !sessionData?.user?.email ? null : (
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={cn('mr-3 flex items-center space-x-1', className)}
+          >
+            <Gravatar
+              className="h-8 w-8 rounded-full"
+              email={sessionData?.user?.email}
+              default="mp"
+            />
+            <div className="flex flex-col">
+              <span className="text-sm font-bold leading-tight">
+                {sessionData?.user?.name}
+              </span>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => {
+                signOut()
+              }}
+              className="flex items-center justify-between"
+            >
+              {' '}
+              <span>Log out</span>
+              <LogoutIcon className="h-4 w-4" />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+    </>
+  )
+}
+
+const Login: React.FC<{className?: string}> = ({className}) => {
+  const {pathname} = useRouter()
+  const {data: sessionData, status: sessionStatus} = useSession()
+  const isLoadingUserInfo = sessionStatus === 'loading'
+
+  return (
+    <>
+      {isLoadingUserInfo || sessionData?.user?.email ? null : (
+        <Link
+          href="/login"
+          className={cn(
+            'hover:opacity-100flex group items-center gap-1 rounded-md px-2.5 py-1 transition',
+            {
+              'underline opacity-100': pathname === '/login',
+              'opacity-75': pathname !== '/login',
+            },
+            className,
+          )}
+        >
+          Log in
+        </Link>
+      )}
+    </>
   )
 }
 
